abf620983e6fb29c306ca4e27f639b25690dac9e
Dmitriy Kucher <dmitriykucher111@gmail.com>
2019-05-28 03:33:13 +0300

    3

--- js/js.js
+++ js/js.js
@@ -26,4 +26,35 @@ function createGallery(gallery) {
 }
 const markupGallery = createGallery(galleryItems);
 const gallery = document.querySelector('.js-gallery');
+const overlay = document.querySelector('.js-overlay');
+const imageOverlay = document.querySelector('.js-imageOverlay');
+const closeModalIcon = document.querySelector('.js-closeModalIcon');
+
 gallery.insertAdjacentHTML('afterbegin', markupGallery);
+gallery.addEventListener('click', viewOriginal);
+overlay.addEventListener('click', closeModal);
+closeModalIcon.addEventListener('click', closeModal);
+window.addEventListener('keydown', closeModalByPress);
+
+function viewOriginal(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  overlay.classList.add('is-visible');
+  imageOverlay.srcset = event.target.dataset.source;
+}
+
+function closeModal(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  overlay.classList.remove('is-visible');
+}
+
+function closeModalByPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  overlay.classList.remove('is-visible');
+}
